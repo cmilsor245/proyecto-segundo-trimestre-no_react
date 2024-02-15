82a5e05786cdabbea82a5a963bd3745829f5e202
cmilsor245 <cmilsor245@g.educaand.es>
2024-02-15 19:47:51 +0100

    cta form

--- js/shared/cta-form/script.js
+++ js/shared/cta-form/script.js
@@ -1,8 +1,19 @@
-const CTA_ACTIVATION_BUTTON = document.querySelector('.cta-btn')
-const CTA_FORM_SECTION = document.querySelector('.cta-form-section')
-const BLUR_OVERLAY = document.querySelector('.cta-blur-overlay')
+const ACTIVATION_BUTTON = document.querySelector('.cta-btn')
+const FORM_SECTION = document.querySelector('.cta-form-section')
+const OVERLAY = document.querySelector('.cta-blur-overlay')
+const RESET_AND_CLOSE_BTN = document.querySelector('.cta-reset')
 
-CTA_ACTIVATION_BUTTON.addEventListener('click', function() {
-  CTA_FORM_SECTION.classList.toggle('hidden')
-  BLUR_OVERLAY.classList.toggle('hidden')
+ACTIVATION_BUTTON.addEventListener('click', function() {
+  FORM_SECTION.classList.remove('hidden')
+  OVERLAY.classList.remove('hidden')
+})
+
+OVERLAY.addEventListener('click', function() {
+  FORM_SECTION.classList.add('hidden')
+  OVERLAY.classList.add('hidden')
+})
+
+RESET_AND_CLOSE_BTN.addEventListener('click', function() {
+  FORM_SECTION.classList.add('hidden')
+  OVERLAY.classList.add('hidden')
 })
