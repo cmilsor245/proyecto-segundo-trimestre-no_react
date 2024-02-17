01817072a48f157489399a837314ba2b94aa4116
cmilsor245 <cmilsor245@g.educaand.es>
2024-02-17 13:55:13 +0100

    enabling cta form access in mobile version

--- js/shared/cta-form/script.js
+++ js/shared/cta-form/script.js
@@ -2,6 +2,7 @@ const ACTIVATION_BUTTON = document.querySelector('.cta-btn')
 const FORM_SECTION = document.querySelector('.cta-form-section')
 const OVERLAY = document.querySelector('.cta-blur-overlay')
 const RESET_AND_CLOSE_BTN = document.querySelector('.cta-reset')
+const ACTIVATION_BUTTON_CTA_SIDE_MENU = document.querySelector('.side--menu-cta-button')
 
 ACTIVATION_BUTTON.addEventListener('click', function() {
   FORM_SECTION.classList.remove('hidden')
@@ -16,4 +17,14 @@ OVERLAY.addEventListener('click', function() {
 RESET_AND_CLOSE_BTN.addEventListener('click', function() {
   FORM_SECTION.classList.add('hidden')
   OVERLAY.classList.add('hidden')
+})
+
+ACTIVATION_BUTTON_CTA_SIDE_MENU.addEventListener('click', function() {
+  SIDE_MENU.classList.remove('show-menu')
+  SIDE_MENU.classList.add('hide-menu')
+  SIDE_MENU_OVERLAY.classList.add('hidden')
+  SIDE_MENU.style.display = 'none'
+
+  FORM_SECTION.classList.remove('hidden')
+  OVERLAY.classList.remove('hidden')
 })
